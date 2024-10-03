6f9e9c015f8cf0b48c266bac9d51260ca98b15ac
ivankadyka <ivankadyko@gmail.com>
2024-10-03 14:43:24 +0200

    CommandLine options added

--- Src/Transformer.CLI/Program.cs
+++ Src/Transformer.CLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
 using Transformer;
 using Transformer.CLI;
 using Transformer.CLI.Options;
@@ -7,17 +8,24 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        //TODO: pass options via CLI args
-        IOptions options = new Options
-        {
-            Input = "testData.json",
-            Output = "outputData.json"
-        };
+        var optionsResult = Parser.Default.ParseArguments<Options>(args);
 
+        if (optionsResult.Tag == ParserResultType.NotParsed)
+        {
+            foreach (var error in optionsResult.Errors)
+            {
+                Console.WriteLine(error);
+            }
+
+            Environment.Exit(-1);
+        } 
+        
+        
         using (var appServiceProvider = AppServiceProvider.Create())
         {
             var gameObjectService = appServiceProvider.GetRequiredService<IGameObjectService>();
-
+            
+            var options = optionsResult.Value;
             await gameObjectService.Transform(options.Input, options.Output);
         }
     }
--- Src/Transformer.CLI/Src/Options/Options.cs
+++ Src/Transformer.CLI/Src/Options/Options.cs
@@ -1,7 +1,21 @@
+using CommandLine;
+
 namespace Transformer.CLI.Options;
 
+/// <summary>
+/// Represents the command-line options for the application.
+/// </summary>
 public class Options : IOptions
 {
+    /// <summary>
+    /// Gets or sets the input file path.
+    /// </summary>
+    [Option('i', "input", Required = true, HelpText = "The path to the input file.")]
     public string Input { get; set; }
+
+    /// <summary>
+    /// Gets or sets the output file path.
+    /// </summary>
+    [Option('o', "output", Required = true, HelpText = "The path to the output file.")]
     public string Output { get; set; }
 }
